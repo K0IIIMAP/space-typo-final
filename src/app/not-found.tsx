a36f35c02763c4b1867b9f51ad499584e7a42355
Mental Lab <admin@MacBook-Air-ADMIN.local>
2024-10-30 00:34:44 +0200

    debuggs

---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+// app/not-found.js
+export default function NotFound() {
+  return (
+    <div style={{ textAlign: "center", padding: "2rem" }}>
+      <h1>Page Not Found</h1>
+      <p>The page youre looking for doesnt exist.</p>
+    </div>
+  );
+}
